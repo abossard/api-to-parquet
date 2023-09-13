9b82a400279e7573379ded4301c431c9702f9d63
Andre Bossard <anbossar@microsoft.com>
2023-09-13 09:09:23 +0200

    latest changes

--- tests/main.go
+++ tests/main.go
@@ -63,7 +63,7 @@ func generateData() InputRecord {
 
 	// Generate three random time series data points
 	var content []TimeSeriesData
-	for i := 0; i < 10000; i++ {
+	for i := 0; i < 80000; i++ {
 		content = append(content, TimeSeriesData{
 			Timestamp:       time.Now().UnixNano() / int64(time.Millisecond),
 			TimeOffsetHours: int8(rand.Intn(24)),
